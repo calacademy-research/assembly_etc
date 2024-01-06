490e08988a3fd06e06aa8b011469866c83837cd7
jbh-cas <jbh@jbhmail.com>
2024-01-05 17:25:32 -0800

    asmdirstruct.sh and links for telo files

--- categories/telomeres/overview_telomere_report.sh
+++ categories/telomeres/overview_telomere_report.sh
@@ -1,1 +1,19 @@
-../../scripts/overview_telomere_report.sh
+#!/bin/bash
+
+# can use the file created by telomere_report.sh or pipe its output to this script to create the overview
+
+awk '
+   BEGIN{tel_typs["TOP"]; tel_typs["TOP_near"]; tel_typs["MIDDLE"]; tel_typs["BOTTOM"]; tel_typs["BOTTOM_near"]}
+   ! ($2 in tel_typs) {
+      next
+   }
+
+   $1 != lst && hit++ {
+      print ""
+   }
+
+  {
+     lst = $1
+     print
+  }
+' $@
