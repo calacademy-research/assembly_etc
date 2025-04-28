d75df7a9ff1995ffd587f4ffe9f9a8b0dcfb7cf1
jbh-cas <jbh@jbhmail.com>
2025-04-28 13:42:46 -0700

    improved blank line skipping

--- scripts/find_taxon_id.sh
+++ scripts/find_taxon_id.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-: look for taxon_id.txt in dir hierarch and if found show first field of first line
+: look for taxon_id.txt in dir hierarchy and if found show first field of first line
 
 taxid_file=$(get_file_in_dir_hierarchy.sh taxon_id.txt)
 
 if [ -s "$taxid_file" ]; then
-   awk '/^#/{next} {print $1; exit}' $taxid_file
+   awk '
+      /^\s*[#:]/ || /^\s*$/ {  # skip blank lines and comment lines
+         next
+      }
+
+      { print $1; exit }
+
+   ' $taxid_file
 else
    echo -e "No taxon_id.txt file found in this or parent directories" >/dev/stderr
 fi
